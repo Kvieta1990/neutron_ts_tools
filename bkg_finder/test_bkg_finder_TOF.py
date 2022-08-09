56db873ed6e8ffbdbc3769a5c02468a4ccd45ace
Yuanpeng Zhang <zyroc1990@gmail.com>
2022-08-09 11:29:09 -0400

    new TOF bkg finder

--- bkg_finder/test_bkg_finder_TOF.py
+++ bkg_finder/test_bkg_finder_TOF.py
@@ -2,17 +2,16 @@ from bkg_finder_TOF import bkg_finder_TOF
 import os
 
 # Testing `bkg_finder` with Si data
-bank_range = [[0., 0.], [900., 12000.], [1400., 20000.], [2000., 18000.], [2000., 14000.], [0., 0.]]
-qmax_bkg_est = [0., 12000., 20000., 18000., 14000., 0.]
-fudge_factor = [1., 1., 0.1, 0.7, 0.7, 1.]
+bank_range = [[1400., 20000.]]
+qmax_bkg_est = [20000.]
+fudge_factor = [1.]
 all_data = []
 for i in range(len(bank_range)):
     x_tmp = []
     y_tmp = []
-    with open(os.path.join("./test_data", "Si_test-" + str(i) + ".xye"), "r") as f:
+    with open(os.path.join("./test_data/20220802", "debug_gsas", "view_bank_" + str(i+2) + "_raw.dat"), "r") as f:
         print("[Info] Reading in file-" + str(i + 1))
-        for j in range(6):
-            line = f.readline()
+        line = "tmp"
         while line:
             line = f.readline()
             if line:
